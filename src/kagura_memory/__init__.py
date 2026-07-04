e8cf8f1cbf03161e16880af5b9c2628b2e94ff3e
Fumikazu Kiyota <fumikazu.kiyota@gmail.com>
2026-07-04 15:40:19 +0900

    chore(release): v0.36.0

--- src/kagura_memory/__init__.py
+++ src/kagura_memory/__init__.py
@@ -95,7 +95,7 @@ from .models import (
 from .resource_client import ResourceClient
 from .workspace_client import WorkspaceClient
 
-__version__ = "0.35.0"
+__version__ = "0.36.0"
 
 __all__ = [
     # Core classes
